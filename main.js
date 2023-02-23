2964464d1a79961c54de1ac287477579a45368fa
colspan <collin@collindigm.com>
2023-02-24 01:43:08 +0300

    printReport finished

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { crawlPage } = require('./crawl.js');
+const { printReport } = require('./report.js');
 
 async function main() {
     /**
@@ -19,9 +20,7 @@ async function main() {
     console.log(`Starting crawl of ${baseURL}`);
     const pages = await crawlPage(baseURL, baseURL, {});
 
-    for (const page of Object.entries(pages)) {
-        console.log(page);
-    }
+    printReport(pages);
 }
 
 main()
---
+++ report.js
@@ -0,0 +1,33 @@
+function printReport(pages) {
+    console.log("======================================");
+    console.log("REPORT");
+    console.log("======================================");
+    const sortedPages = sortPages(pages);
+
+    for (const sortedPage of sortedPages) {
+        const url = sortedPage[0];
+        const hits = sortedPage[1];
+        console.log(`Found ${hits} links to page: ${url}`);
+    }
+
+    console.log("======================================");
+    console.log("END REPORT");
+    console.log("======================================");
+}
+
+function sortPages(pages) {
+    const pageArr = Object.entries(pages);
+
+    pageArr.sort((a, b) => {
+        aHits = a[1];
+        bHits = b[1];
+        return b[1] - a[1];     /** From greatest to smallest */
+    })
+
+    return pageArr;
+}
+
+module.exports = {
+    sortPages,
+    printReport
+} 
